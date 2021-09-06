48dae5831ae4deadb6bcf7be77f70b0380989fae
issasoftdev <dsmzkali@yahoo.com>
2021-09-06 19:57:46 +0200

    Update navigation across entire website

--- JS/main.js
+++ JS/main.js
@@ -2,16 +2,27 @@ const menuBtn = document.getElementById("menuBtn");
 const menuExitBtn = document.getElementById("menuExitBtn");
 const portfolioMenu = document.getElementById("portfolioMenu");
 const header = document.getElementById("header");
+const graphicDesignMenuButton = document.getElementById("graphicDesignMenu");
+const graphicSubMenu = document.getElementById("graphicSubMenu");
 
 function openMenu() {
-    portfolioMenu.style.display = "block";
-    menuBtn.style.display = "none";
+    portfolioMenu.classList.remove("hidden");
+    menuBtn.classList.add("hidden"); 
 };
 
 function closeMenu() {
-    portfolioMenu.style.display = "none";
-    menuBtn.style.display = "block";
+    portfolioMenu.classList.add("hidden");
+    menuBtn.classList.remove("hidden"); 
 };
+
+function openGraphicMenu() {
+    if (graphicSubMenu.classList.contains("hidden")) {
+        graphicSubMenu.classList.remove("hidden");
+    } else {
+        graphicSubMenu.classList.add("hidden");
+    } 
+}
 
 menuBtn.addEventListener('click', openMenu);
 menuExitBtn.addEventListener('click', closeMenu);
+graphicDesignMenuButton.addEventListener('click', openGraphicMenu);
